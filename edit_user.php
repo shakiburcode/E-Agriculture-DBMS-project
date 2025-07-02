4f562a8b9a9d482d990915680a55b10595bafa24
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-07-02 19:42:07 +0600

    Update edit_user.php

--- edit_user.php
+++ edit_user.php
@@ -2,6 +2,7 @@
 //Start the session to see if the user is authenticated user. 
 
 
+
 	session_start();
 	//Check if the user is authenticated first. Or else redirect to login page 
 	if(isset($_SESSION['IS_AUTHENTICATED']) && $_SESSION['IS_AUTHENTICATED'] == 1){
