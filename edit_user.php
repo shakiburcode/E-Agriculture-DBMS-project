c6eefe415ec5095a0d2dbc2d8894bd4358e6ba45
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-10-28 06:32:25 +0600

    Clean up blank lines in edit_user.php
    Removed unnecessary blank lines at the beginning of the file.

--- edit_user.php
+++ edit_user.php
@@ -1,7 +1,5 @@
 <?php
 //Start the session to see if the user is authenticated user. 
-
-
 	session_start();
 	//Check if the user is authenticated first. Or else redirect to login page 
 	if(isset($_SESSION['IS_AUTHENTICATED']) && $_SESSION['IS_AUTHENTICATED'] == 1){
